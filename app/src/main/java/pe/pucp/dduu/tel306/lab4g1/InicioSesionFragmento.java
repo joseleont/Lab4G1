b9e018d1b966a10f08b1d37ba002f4356c4f315e
“José <jose.leont@pucp.pe>
2020-11-24 09:02:41 -0500

    Fragmentos

--- app/src/main/java/pe/pucp/dduu/tel306/lab4g1/InicioSesionFragmento.java
+++ app/src/main/java/pe/pucp/dduu/tel306/lab4g1/InicioSesionFragmento.java
@@ -2,6 +2,8 @@ package pe.pucp.dduu.tel306.lab4g1;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,39 +33,15 @@ public class InicioSesionFragmento extends Fragment {
 
     }
 
-    private AlertDialog crearDialogo() {
-        AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater=getActivity().getLayoutInflater();
-        View v = inflater.inflate(R.layout.fragment_inicio_sesion_fragmento,null);
-        builder.setView(v);
 
-        Button btnAceptar=v.findViewById(R.id.btnIniciarSesion);
-
-
-        btnAceptar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                int error=0;
-
-                //OBTENER LA INFORMACION DE LOS VIEWS
-
-
-
-            }
-        });
-
-
-        return builder.create();
-
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-      // return crearDialogo();
         return inflater.inflate(R.layout.fragment_inicio_sesion_fragmento, container, false);
     }
+
+
 }
